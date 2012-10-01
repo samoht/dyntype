61644d0d14ca27ff60e724199a1e443cb868c70e
Anil Madhavapeddy <anil@recoil.org>
2012-10-01 11:11:40 -0700

    make `post_conf.sh` more portable by using `/bin/sh` instead of `bash`

--- post_conf.sh
+++ post_conf.sh
@@ -1,6 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/sh -e
 
 PP_DIR=`ocamlfind query type_conv -predicates syntax,preprocessor -r -format "-I %d %a"`
 
 echo "pp_command=\"camlp4o $PP_DIR\"" >> setup.data
-
